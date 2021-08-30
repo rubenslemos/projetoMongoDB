d023805f1e9628f0b3ee30ff82a4b53ee1346d0a
rubenslemos <rubenslemos@gmail.com>
2021-08-30 00:42:33 -0300

    change

--- models/db.js
+++ models/db.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose')
 if (process.env.NODE_ENV == "production") {
-    mongoose.connect("mongodb+srv://rubenslemos:rumos1982@blogdonode.kxnmn.mongodb.net/blogdonode?retryWrites=true&w=majority"
-            // , {
-            //         useNewUrlParser: true,
-            //         useUnifiedTopology: true,
-            //     }
-        )
+    mongoose.connect("mongodb+srv://rubenslemos:rumos1982@blogdonode.kxnmn.mongodb.net/blogdonode?retryWrites=true&w=majority", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
         .then(() => {
             console.log("MongoDB Conectado...")
         }).catch((err) => {
